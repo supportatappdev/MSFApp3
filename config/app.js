79e0c8df1cf804c4aa5b7a2b29dc5938d485c448
Support <5upportatappdev@gmail.com>
2017-06-14 21:30:10 +0530

    Initial Checkin

---
+++ config/app.js
@@ -0,0 +1,23 @@
+/**
+ * mymobile3
+ */
+(function () {
+    angular.module('mymobile3', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',                 // Ui Bootstrap
+         'cgNotify',
+          'oitozero.ngSweetAlert' 
+    ])
+})();
+
+
+function getBaseURL() {
+	   return location.protocol + "//" + location.hostname + 
+	      (location.port && ":" + location.port) ;
+}; 
+function getAppName(p) {
+   return "/";
+}
+var _appUrl = "http://ec2-54-80-147-67.compute-1.amazonaws.com:8180/";//getBaseURL()+getAppName(window.location.pathname);
+
