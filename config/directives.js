b47b97c64b36565149db7b17f3f81dda47844699
Support <5upportatappdev@gmail.com>
2017-06-16 16:59:47 +0530

    Updated on Fri Jun 16 11:29:35 UTC 2017

--- config/directives.js
+++ config/directives.js
@@ -6,9 +6,9 @@ function pageTitle($rootScope, $timeout) {
         link: function(scope, element) {
             var listener = function(event, toState, toParams, fromState, fromParams) {
                 // Default title - load on Dashboard 1
-                var title = 'mymobile3 | Responsive Admin Theme';
+                var title = 'Journey Plan';
                 // Create your own title pattern
-                if (toState.data && toState.data.pageTitle) title = 'mymobile3 | ' + toState.data.pageTitle;
+                if (toState.data && toState.data.pageTitle) title =  toState.data.pageTitle;
                 $timeout(function() {
                     element.text(title);
                 });
