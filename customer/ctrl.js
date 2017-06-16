0384d338f0113dca51a301eea11fd616e1251033
Support <5upportatappdev@gmail.com>
2017-06-16 17:48:52 +0530

    Updated on Fri Jun 16 12:18:31 UTC 2017

---
+++ customer/ctrl.js
@@ -0,0 +1,15 @@
+/**
+ * CustCtrl - controller
+ */
+angular
+    .module('mymobile3')
+    .controller('CustCtrl', function MainCtrl($scope,Cache,$location,AlertService,$http) {
+    
+    });
+    
+angular
+    .module('mymobile3')
+    .controller('AddCustCtrl', function MainCtrl($scope,Cache,$location,AlertService,$http) {
+    
+    });    
+    
