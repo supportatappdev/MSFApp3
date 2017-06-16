d042ec063d8973c4a5b6ff5ca79b66377f02f664
Support <5upportatappdev@gmail.com>
2017-06-16 21:38:55 +0530

    Updated on Fri Jun 16 16:08:33 UTC 2017

--- customer/ctrl.js
+++ customer/ctrl.js
@@ -1,10 +1,18 @@
 /**
  * CustCtrl - controller
  */
+//controller('OrderCtrl',function(Cache,$scope,$location,BSServiceUtil,BSService,Util,AlertService,$modal){
+ 
 angular
     .module('mymobile3')
-    .controller('CustCtrl', function MainCtrl($scope,Cache,$location,AlertService,$http) {
+    .controller('CustCtrl', function MainCtrl($scope,Cache,$location,AlertService,$http,BSServiceUtil) {
         $("body").removeClass("mini-navbar");
+        var spRetailResult = function(result) {
+            $scope.spRetailList = result;
+        }
+        var wc = "spid = 11";//sp.salesperson
+        //var wcParams = [parseInt($scope.sp.salesperson)];
+        BSServiceUtil.queryResultWithCallback("SFSPRetailViewRef", "_NOCACHE_", wc, undefined, undefined, spRetailResult);
     });
     
 angular
