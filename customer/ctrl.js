e91fa5c476e85e76aad854574592e2f0fff03d0c
Support <5upportatappdev@gmail.com>
2017-06-16 18:29:09 +0530

    Updated on Fri Jun 16 12:58:48 UTC 2017

--- customer/ctrl.js
+++ customer/ctrl.js
@@ -4,12 +4,12 @@
 angular
     .module('mymobile3')
     .controller('CustCtrl', function MainCtrl($scope,Cache,$location,AlertService,$http) {
-    
+        $("body").removeClass("mini-navbar");
     });
     
 angular
     .module('mymobile3')
     .controller('AddCustCtrl', function MainCtrl($scope,Cache,$location,AlertService,$http) {
-    
+        $("body").removeClass("mini-navbar");
     });    
     
