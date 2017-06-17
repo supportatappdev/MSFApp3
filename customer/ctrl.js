e7d8249d0fc87a0a5ac37c5ad21497bc2ce493d6
Support <5upportatappdev@gmail.com>
2017-06-17 23:52:02 +0530

    Updated on Sat Jun 17 18:21:42 UTC 2017

--- customer/ctrl.js
+++ customer/ctrl.js
@@ -22,7 +22,44 @@ angular
 
 angular
     .module('mymobile3')
-    .controller('AddCustCtrl', function MainCtrl($scope,Cache,$location,AlertService,$http) {
+    .controller('AddCustCtrl', function MainCtrl($scope,Cache,$location,AlertService,$http,BSService) {
         $("body").removeClass("mini-navbar");
+        var _operation = 'INSERT';
+        $scope.addCustomer = function() {
+        $scope.addcspinner = true;
+          var inputJSON = {};
+                if(_operation !== 'UPDATE') {
+                    inputJSON.uid = Cache.loggedInUser.userId;
+                } else {
+                    inputJSON.CREATION_DATE = Util.convertDBDate(inputJSON.CREATION_DATE);
+                    inputJSON.LAST_UPDATE_DATE = Util.convertDBDate(new Date());
+                }
+                inputJSON = $scope.cust;
+                inputJSON.cust_code = 'W00'+$scope.cust.cust_name;
+                 var params = {
+                'ds': 'FISFCustomerRef',
+                'operation': _operation,
+                'data': inputJSON
+            };
+            BSService.save({
+                'method': 'update'
+            }, params, function(result) {
+                if (result.status === "E") {
+                    AlertService.showError("Validation Error",result.errorMsg);
+                }  else {
+                    var _op = "Added";
+                    if(_operation == 'UPDATE') {
+                        _op = "Updated";
+                    }
+                    AlertService.showInfo("Added retailer ["+$scope.cust.cust_name+"]  successfully");
+                    $scope.gotoCustomers();
+                }
+                $scope.addcspinner  =false;
+            });
+        }
+        
+        $scope.gotoCustomers = function() {
+            $location.path("/index/listcust");
+        }
     });    
     
