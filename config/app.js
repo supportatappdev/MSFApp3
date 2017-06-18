4603c5f8bc8da7c83df504f4488a0bbe95d45fea
Support <5upportatappdev@gmail.com>
2017-06-18 12:02:23 +0530

    Updated on Sun Jun 18 06:32:14 UTC 2017

--- config/app.js
+++ config/app.js
@@ -8,7 +8,8 @@
         'ui.bootstrap',                 // Ui Bootstrap
         'ngResource',
          'cgNotify',
-          'oitozero.ngSweetAlert' 
+          'oitozero.ngSweetAlert',
+          'infinite-scroll'
     ])
 })();
 
